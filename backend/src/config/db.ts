079a30f0b8ef611f41c960ff410023a1ca181bdb
Paul Cellano <paul.cellano@hireplicity.com>
2026-01-16 01:37:53 +0800

    added connection string

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -4,6 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_NAME || "sari_sari_store",
